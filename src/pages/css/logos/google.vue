0af21d6c6e80a5ea54e2cacad93ad3f7110d9031
Ricardo <rvw_po@mac.com>
2022-01-19 23:08:35 +0100

    css logo google

---
+++ src/pages/css/logos/google.vue
@@ -0,0 +1,153 @@
+<script setup lang="ts">
+import { ref, computed, unref } from 'vue';
+
+const step = ref(0)
+const max = 13
+
+const outlineSteps = computed(() => unref(step) >= 5 ? 5 : unref(step) )
+const halfSteps = computed(() => unref(step) >= 10 ? 10 : unref(step) )
+
+const btnLabel = computed(() => {
+  if (unref(step) === max) return 'Done!'
+  return `Next ${unref(step)}/${max}`
+})
+</script>
+
+<template>
+  <!-- google logo final -->
+  <div v-if="step === 0" class="w-full h-screen
+    flex flex-col justify-center items-center">
+    <button @click="step++" class="mb-5">Start</button>
+    <div class="google">
+      <div class="base final-outline"></div>
+      <div class="base final-half"></div>
+      <div class="final-line"></div>
+    </div>
+  </div>
+  <!-- google logo steps -->
+  <div v-if="step > 0" class="w-full h-screen
+    flex flex-col justify-center items-center">
+    <button @click="step++" class="mb-5"
+      :disabled="step === max">
+      {{ btnLabel }}
+    </button>
+    <div class="google">
+      <div class="base transition-all duration-300" :class="`outline-step-${outlineSteps}`"></div>
+      <div class="base transition-all duration-300" :class="`half-step-${halfSteps}`"></div>
+      <div class="transition-all duration-300" :class="`line-step-${step}`"></div>
+    </div>
+  </div>
+</template>
+
+<style>
+.google {
+  position: relative;
+  width: 150px;
+  height: 150px;
+}
+.base {
+  position: absolute;
+  width: 150px;
+  height: 150px;
+}
+.final-outline {
+  border-top: 30px solid #E33E2B;
+  border-right: 30px solid transparent;
+  border-bottom: 30px solid #2BA14B;
+  border-left: 30px solid #F1B500;
+  border-radius: 50%;
+}
+.final-half {
+  border: 30px solid transparent;
+  border-right: 30px solid #3A7CEC;
+  transform: rotate(45deg);
+  border-radius: 50%;
+  z-index: -1;
+}
+.final-line {
+  position: absolute;
+  width: 80px;
+  height: 30px;
+  background-color: #3A7CEC;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  border-bottom-right-radius: 15px;
+}
+/* BUILD STEPS */
+.outline-step-1 {
+  border-top: 30px solid #E33E2B;
+}
+.outline-step-2 {
+  border-top: 30px solid #E33E2B;
+  border-bottom: 30px solid #2BA14B;
+}
+.outline-step-3 {
+  border-top: 30px solid #E33E2B;
+  border-bottom: 30px solid #2BA14B;
+  border-left: 30px solid #F1B500;
+}
+.outline-step-4 {
+  border-top: 30px solid #E33E2B;
+  border-bottom: 30px solid #2BA14B;
+  border-left: 30px solid #F1B500;
+  border-right: 30px solid transparent;
+}
+.outline-step-5 {
+  border-top: 30px solid #E33E2B;
+  border-bottom: 30px solid #2BA14B;
+  border-left: 30px solid #F1B500;
+  border-right: 30px solid transparent;
+  border-radius: 50%;
+}
+.half-step-6 {
+  border-right: 30px solid #3A7CEC;
+}
+.half-step-7 {
+  border: 30px solid transparent;
+  border-right: 30px solid #3A7CEC;
+}
+.half-step-8 {
+  border: 30px solid transparent;
+  border-right: 30px solid #3A7CEC;
+  transform: rotate(45deg);
+}
+.half-step-9 {
+  border: 30px solid transparent;
+  border-right: 30px solid #3A7CEC;
+  transform: rotate(45deg);
+  border-radius: 50%;
+}
+.half-step-10 {
+  border: 30px solid transparent;
+  border-right: 30px solid #3A7CEC;
+  transform: rotate(45deg);
+  border-radius: 50%;
+  z-index: -1;
+}
+.line-step-11 {
+  position: absolute;
+  width: 80px;
+  height: 30px;
+  background-color: #3A7CEC;
+}
+.line-step-12 {
+  position: absolute;
+  width: 80px;
+  height: 30px;
+  background-color: #3A7CEC;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+.line-step-13 {
+  position: absolute;
+  width: 80px;
+  height: 30px;
+  background-color: #3A7CEC;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  border-bottom-right-radius: 15px;
+}
+</style>
