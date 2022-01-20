6e384404bfb99c6082d9d768a41bab63a5a8c74a
Ricardo <rvw_po@mac.com>
2022-01-20 21:02:06 +0100

    css logo adidas

---
+++ src/pages/css/logos/adidas/index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <!-- adidas logo -->
+  <div class="w-full h-screen
+    flex flex-col justify-center items-center">
+    <button @click="$router.push({ name: 'logo-adidas-steps' })"
+      class="mb-5">Start</button>
+    <div class="adidas">
+      <div class="line one"></div>
+      <div class="line two"></div>
+      <div class="line three"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.adidas {
+  position: relative;
+  width: 206px;
+  height: 120px;
+  overflow: hidden;
+}
+.line {
+  width: 46px;
+  height: 150px;
+  background-color: #111;
+  position: absolute;
+  transform: rotate(-31deg)
+}
+.one {
+  top: 86px;
+  left: 36px;
+}
+.two {
+  top: 45px;
+  left: 82px;
+}
+.three {
+  top: 4px;
+  left: 129px;
+}
+</style>
